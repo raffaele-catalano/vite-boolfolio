a2381faac934505dc868f2ccff8d32cc1f456ccc
raffaele-catalano <raf.catalano@gmail.com>
2023-06-27 09:50:03 +0200

    created Main component - injected in App

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,18 @@
 <script>
-
-import {store} from './data/store.js'
-import axios from 'axios';
+import Main from "./components/Main.vue"
 
 export default {
   name: 'App',
 
-  data(){
-    return {
-      projects: []
-    }
-  },
-
-  methods: {
-    getApi(){
-      axios.get(store.apiUrl + 'projects')
-        .then(results => {
-          this.projects = results.data;
-        })
-    }
-  },
-
-  mounted(){
-    this.getApi();
+  components: {
+    Main,
   }
 
 }
 </script>
 
 <template>
-  <div class="container bg-dark text-light">
-    <h1 class="text-warning">Project List:</h1>
-    <ul>
-      <li
-        v-for="project in projects"
-        :key="project.id"
-        >
-        <span>{{ project.name }}</span>
-      </li>
-    </ul>
-  </div>
+  <Main/>
 </template>
 
 
---
+++ src/components/Main.vue
@@ -0,0 +1,47 @@
+<script>
+import axios from 'axios';
+import {store} from '../data/store.js';
+
+export default {
+name: 'Main',
+
+data() {
+    return {
+        store,
+        projects: []
+    }
+},
+
+methods: {
+    getApi() {
+        axios.get(store.apiUrl + 'projects')
+            .then(results => {
+                this.projects = results.data;
+            })
+    }
+},
+
+mounted() {
+    this.getApi();
+}
+}
+</script>
+
+<template>
+    <div class="container bg-dark text-light">
+        <h1 class="text-warning">Project List:</h1>
+        <ul>
+            <li
+            v-for="project in projects"
+            :key="project.id"
+            >
+                <span>{{ project.name }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
