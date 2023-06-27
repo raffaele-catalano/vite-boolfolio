10eed0c068d9a765b54694f542419efecb2ad36d
raffaele-catalano <raf.catalano@gmail.com>
2023-06-27 16:14:57 +0200

    created Home&Projects Pages: - moved logic and template from Main in Projects - moved template in Home

---
+++ src/pages/Home.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<template>
+    <div class="jumbotron container my-3">
+            <h1 class="text-center text-primary fw-bold my-4">
+                Welcome to Boolfolio
+            </h1>
+    
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quidem dignissimos nihil repudiandae quos harum placeat distinctio voluptatum itaque commodi, iusto, eveniet eius. Quisquam labore animi possimus consequuntur dicta iusto maxime numquam debitis cum, nemo, aperiam et! Aperiam itaque labore quia aliquid qui nemo!</p>
+            <div class="gif-container d-flex justify-content-center">
+                <img class="w-25" src="https://upload.wikimedia.org/wikipedia/commons/8/81/Portfolio_.gif" alt="portfolio-gif">
+            </div>
+        </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/pages/Projects.vue
@@ -0,0 +1,63 @@
+<script>
+import axios from 'axios';
+import {store} from '../data/store.js';
+
+import Card from '../components/partials/Card.vue'
+
+export default {
+    name: 'Projects',
+
+    components: {
+        Card,
+    },
+    
+    data() {
+        return {
+            store,
+            projects: [],
+            results: []
+        }
+    },
+
+    methods: {
+        getApi() {
+            axios.get(store.apiUrl + 'projects')
+            
+                .then(results => {
+
+                    this.projects = results.data;
+                    this.results = results.data;
+
+                    console.log(this.projects);
+                    console.log(results.data);
+                })
+        }
+    },
+
+
+    mounted() {
+        this.getApi();
+    }
+
+}
+</script>
+
+<template>
+    <div class="container my-3">
+        <h2 class="text-center text-primary fw-bold">Projects List</h2>
+        <p class="py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque numquam illum optio sunt praesentium iste, dolores obcaecati veritatis impedit sapiente ab autem voluptates mollitia quisquam minus repudiandae quidem, animi placeat.</p>
+        <div class="row row-cols-5 py-2">
+            <Card
+            v-for="project in projects"
+            :key="project.id"
+            :title="project.name"
+            :type="project.type.name"
+            :technologies="project.technologies"
+            :status="project.is_closed"/>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
