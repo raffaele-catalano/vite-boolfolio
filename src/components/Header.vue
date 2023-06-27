67bc523edd7f661d4d1381d6214c7769e2ac29d4
raffaele-catalano <raf.catalano@gmail.com>
2023-06-27 16:13:14 +0200

    made Router operations: - setup router-view in Main - setup router-link in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,14 +15,19 @@ export default {
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse ms-4" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    <li class="nav-item mx-2">
+                        <router-link :to="{name: 'home'}"><i class="fa-solid fa-house-chimney fa-lg"><span class="page_name text-dark">Home</span></i></router-link>
+                        <!-- <a class="nav-link" aria-current="page" href="#">Home</a> -->
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mx-2">
+                        <router-link :to="{name: 'projects'}"><i class="fa-solid fa-list-check fa-lg"><span class="page_name text-dark">Projects</span></i></router-link>
+                        <!-- <a class="nav-link" aria-current="page" href="#">Contacts</a> -->
+                    </li>
+                    <!-- <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="#">Admin</a>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
         </div>
@@ -31,5 +36,28 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+    header {
+        .navbar {
+            .container-fluid {
+                .navbar-collapse {
+                    .nav-item {
+                        &:hover {
+                            transform: scale(1.2);
+                            transition: all 0.3s;
+                        }
+                        i {
+                            .page_name {
+                                font-size: .7rem;
+                                margin-left: 5px;
+                            }
 
+                            &.fa-list-check {
+                                margin-top: 13px;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,70 +1,21 @@
 <script>
-import axios from 'axios';
-import {store} from '../data/store.js';
-
-import Card from './partials/Card.vue'
+import Home     from '../pages/Home.vue'
+import Projects from '../pages/Projects.vue'
 
 export default {
     name: 'Main',
 
     components: {
-        Card,
-    },
-
-    data() {
-        return {
-            store,
-            projects: [],
-            results: []
-        }
-    },
-
-    methods: {
-        getApi() {
-            axios.get(store.apiUrl + 'projects')
-            
-                .then(results => {
-
-                    this.projects = results.data;
-                    this.results = results.data;
-
-                    console.log(this.projects);
-                    console.log(results.data);
-                })
-        }
-    },
-
-
-    mounted() {
-        this.getApi();
+        Home,
+        Projects,
     }
 }
 </script>
 
 <template>
+    
     <main>
-        <div class="jumbotron container my-3">
-            <h1 class="text-center text-primary fw-bold my-4">
-                Welcome to Boolfolio
-            </h1>
-    
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quidem dignissimos nihil repudiandae quos harum placeat distinctio voluptatum itaque commodi, iusto, eveniet eius. Quisquam labore animi possimus consequuntur dicta iusto maxime numquam debitis cum, nemo, aperiam et! Aperiam itaque labore quia aliquid qui nemo!</p>
-            <div class="gif-container d-flex justify-content-center">
-                <img class="w-25" src="https://upload.wikimedia.org/wikipedia/commons/8/81/Portfolio_.gif" alt="portfolio-gif">
-            </div>
-        </div>
-    
-        <div class="container">
-                <div class="row row-cols-5 py-2">
-                    <Card
-                    v-for="project in projects"
-                    :key="project.id"
-                    :title="project.name"
-                    :type="project.type.name"
-                    :technologies="project.technologies"
-                    :status="project.is_closed"/>
-                </div>
-        </div>
+        <router-view></router-view>
     </main>
 
 </template>
