dea804d4e43e0a95b3c29926e222b9dc1dbe3cd1
raffaele-catalano <raf.catalano@gmail.com>
2023-06-27 15:12:57 +0200

    created-compiled-imported router.js

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Home     from './pages/Home.vue';
+import Projects from './pages/Projects.vue';
+
+const router = createRouter ({
+    history: createWebHistory(),
+
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: Projects
+        }
+    ]
+})
+
+export {router};
