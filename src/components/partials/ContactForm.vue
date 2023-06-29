105a736b4fba440873f1dd692ac581a8c15644a1
raffaele-catalano <raf.catalano@gmail.com>
2023-06-29 20:22:39 +0200

    added sendForm function linked to v-model of form

--- src/components/partials/ContactForm.vue
+++ src/components/partials/ContactForm.vue
@@ -3,29 +3,49 @@ import axios from 'axios';
 import { store } from '../../data/store';
 
 export default {
-    name: 'ContactForm'
+    name: 'ContactForm',
+
+    data(){
+        return{
+            name: '',
+            email: '',
+            message: '',
+        }
+    },
+
+    methods: {
+        sendForm(){
+            const data = {
+                name: this.name,
+                email: this.email,
+                message: this.message
+            }
+            console.log(data);
+        }
+    }
 }
 </script>
 
 <template>
 
     <div class="container rc-form rounded-2">
-        <form action="" class="p-3">
+        <!-- blocca la ricarica della pagina che di base è automatica al click di un button type=submit -->
+        <form @submit.prevent="sendForm()" class="p-3">
     
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
-                <input type="text" name="name" id="" class="form-control" placeholder="Type your Fullname">
+                <input v-model.trim="name" type="text" name="name" id="" class="form-control" placeholder="Type your Fullname">
             </div>
             
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" id="" class="form-control" placeholder="Type your Email">
+                <input v-model.trim="email" type="email" name="email" id="" class="form-control" placeholder="Type your Email">
             </div>
     
             <div class="form-floating mb-4">
                 <p>Message</p>
                 <!-- <label for="message" class="form-label">Message</label> -->
-                <textarea name="message" id="" class="form-control" style="height: 150px; width: 100%;"></textarea>
+                <textarea v-model.trim="message" name="message" id="" class="form-control" style="height: 150px; width: 100%;"></textarea>
             </div>
             
             <div class="d-flex justify-content-center">
@@ -37,6 +57,6 @@ export default {
 
 <style lang="scss" scoped>
     .rc-form {
-        box-shadow: 0 0 10px rgba($color: #000000, $alpha: 0.2);
+        box-shadow: 0 0 10px rgba($color: #000000, $alpha: 0.1);
     }
 </style>
