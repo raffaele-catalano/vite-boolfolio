93f6c90b44bb106c7a2f6bed1ea5700119f67575
raffaele-catalano <raf.catalano@gmail.com>
2023-06-29 21:37:30 +0200

    added errors message + important fix

--- src/components/partials/ContactForm.vue
+++ src/components/partials/ContactForm.vue
@@ -27,7 +27,14 @@ export default {
                 .then(result => {
 
                     this.errors = result.data.errors;
+
+                    if(!result.data.success) {
+                        this.errors = result.data.errors;
+                    }else {
+                        this.errors = {};
+                    }
                     console.log(result.data);
+                    console.log(result.data.errors);
 
                 })
         }
@@ -44,16 +51,19 @@ export default {
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
                 <input v-model.trim="name" :class="{ 'is-invalid' : errors.name }" type="text" class="form-control" placeholder="Type your Fullname">
+                <p v-for="(error, index) in errors.name" :key="index" class="text-danger fs-6 ms-2"><i class="fa-solid fa-circle-exclamation"></i> {{ error }}</p>
             </div>
             
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input v-model.trim="email" :class="{ 'is-invalid' : errors.email }" type="email" class="form-control" placeholder="Type your Email">
+                <p v-for="(error, index) in errors.email" :key="index" class="text-danger fs-6 ms-2"><i class="fa-solid fa-circle-exclamation"></i> {{ error }}</p>
             </div>
     
             <div class="form-floating mb-4">
                 <p>Message</p>
                 <textarea v-model.trim="message" :class="{ 'is-invalid' : errors.message }" class="form-control" style="height: 150px; width: 100%;"></textarea>
+                <p v-for="(error, index) in errors.message" :key="index" class="text-danger fs-6 ms-2"><i class="fa-solid fa-circle-exclamation"></i> {{ error }}</p>
             </div>
             
             <div class="d-flex justify-content-center">
@@ -67,4 +77,9 @@ export default {
     .rc-form {
         box-shadow: 0 0 10px rgba($color: #000000, $alpha: 0.1);
     }
+
+    // .error-msg {
+    //     color: #DC3545;
+    //     font-se
+    // }
 </style>
