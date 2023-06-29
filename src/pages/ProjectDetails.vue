11c72e23f74c039d1534d928fb07cbf4928f448c
raffaele-catalano <raf.catalano@gmail.com>
2023-06-29 18:16:56 +0200

    created & implemented ProjectDetail page

---
+++ src/pages/ProjectDetails.vue
@@ -0,0 +1,82 @@
+<script>
+import axios from 'axios';
+import {store} from '../data/store.js';
+import Loader from '../components/partials/Loader.vue'
+
+export default {
+    name: 'ProjectDetails',
+
+    components: {
+        Loader,
+    },
+
+    data() {
+        return {
+            store,
+            loaded:false,
+            project: null,
+            technologies: [],
+        }
+    },
+
+    methods:{
+        getApi(){
+            this.loaded = false;
+            axios.get(store.apiUrl + 'projects/details/' + this.$route.params.slug)
+                .then(results =>{
+                    this.project = results.data;
+                    this.loaded = true;
+                    console.log(this.project);
+                })
+        },
+
+    },
+
+    mounted(){
+        this.getApi()
+    }
+
+}
+
+</script>
+
+<template>
+    <Loader v-if="!loaded"/>
+    <div v-else class="container my-3">
+            <h2 class="text-center text-primary fw-bold my-4">
+                <span class="text-primary">Project:</span> {{ project.name }}
+            </h2>
+            <!-- FIX -->
+            <!-- <div class="container d-flex justify-content-center mb-2">
+                <img :src=project.image_path class="w-25">
+            </div> -->
+            <h5>
+                <span class="text-primary">Category:</span> {{ project.category }}
+            </h5>
+            <h5>
+                <span class="text-primary">Start Date:</span> {{ project.start_date }}
+            </h5>
+            <h5>
+                <span class="text-primary">End Date:</span> {{ project.end_date }}
+            </h5>
+            <h5>
+                <span class="text-primary">Type:</span> {{ project.type.name }}
+            </h5>
+            <h5>
+                <span class="text-primary">Technologies: </span>
+                <span v-for="technology in project.technologies" :key="technology.id" class="badge text-bg-warning me-2">{{ technology.name }}</span>
+                <span class="badge text-bg-danger me-2" v-if="!project.technologies.length">Undefined</span>
+            </h5>
+
+
+
+            <div class="text-center py-3">
+                <router-link  class="btn btn-success" :to="{'name' : 'projects'}">Back to view</router-link>
+            </div>
+    
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
